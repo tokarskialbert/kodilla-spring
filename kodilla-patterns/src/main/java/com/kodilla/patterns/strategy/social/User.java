fa6c1356097a7af3ebcfd6801c58b8f111812c64
Albert Tokarski <tokarskialbert@gmail.com>
2020-04-30 20:15:05 +0200

    strategy pattern

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+
+    private String name;
+    protected SocialPublisher socialPublisher;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String sharePost() {
+
+        return "sharing post: " + socialPublisher.share();
+    }
+
+    public void setSocialPublisher(SocialPublisher socialPublisher) {
+
+        this.socialPublisher = socialPublisher;
+    }
+}
